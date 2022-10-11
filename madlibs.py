df8303b2b65880b7cb5d35c1e2673a04a2d6cb50
“Julia <juliaabkhan@gmail.com>
2022-10-11 15:51:23 -0500

    Completed main exercise.
    Co-Authored-By leyasalazar 96083374+leyasalazar@users.noreply.github.com

--- madlibs.py
+++ madlibs.py
@@ -50,6 +50,27 @@ def greet_person():
 
     return render_template("compliment.html", person=player, compliment=compliment)
 
+@app.route("/game")
+def show_madlib_form():
+    """Shows a madlib form"""
+    answer = request.args.get('play_game')
+    
+    if answer == "True":
+        return render_template("game.html") #show madlib
+    else:
+        return render_template("goodbye.html")
+
+@app.route("/madlib")
+def show_madlib():
+    """Shows the output from the madlib form"""
+
+    person = request.args.get('person')
+    color = request.args.get('color')
+    noun = request.args.get('noun')
+    adjective = request.args.get('adjective')
+
+    return render_template("madlib.html", person=person, color=color, noun=noun, adjective=adjective)
+    
 
 if __name__ == "__main__":
     # Setting debug=True gives us error messages in the browser and also
