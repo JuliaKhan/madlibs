cb6dd651b2246c0d0e0b0478d14e490f5f96eb58
“Julia <juliaabkhan@gmail.com>
2022-10-12 12:03:54 -0500

    Upgraded templates with inheritance from base.html and added session cookies to track a username over multiple pages.
    Co-Authored-By: leyasalazar 96083374+leyasalazar@users.noreply.github.com

--- madlibs.py
+++ madlibs.py
@@ -1,12 +1,16 @@
 """A madlib game that compliments its users."""
 
+from crypt import methods
 from random import choice
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 
 # "__name__" is a special Python variable for the name of the current module.
 # Flask wants to know this to know what any imported things are relative to.
 app = Flask(__name__)
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 AWESOMENESS = [
     "awesome",
@@ -27,10 +31,31 @@ AWESOMENESS = [
 
 
 @app.route("/")
-def start_here():
+def index():
     """Display homepage."""
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return 'You are not logged in'
+    # return "Hi! This is the home page."
 
-    return "Hi! This is the home page."
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 
 @app.route("/hello")
@@ -40,15 +65,16 @@ def say_hello():
     return render_template("hello.html")
 
 
-@app.route("/greet")
+@app.route("/greet", methods=['post'])
 def greet_person():
     """Greet user with compliment."""
 
-    player = request.args.get("person")
+    # person = request.form.get("person")   #uses form answer
+    person = session['username']            #uses session username
 
     compliment = choice(AWESOMENESS)
 
-    return render_template("compliment.html", person=player, compliment=compliment)
+    return render_template("compliment.html", person=person, compliment=compliment)
 
 @app.route("/game")
 def show_madlib_form():
@@ -56,9 +82,9 @@ def show_madlib_form():
     answer = request.args.get('play_game')
     
     if answer == "True":
-        return render_template("game.html") #show madlib
+        return render_template("game.html")
     else:
-        return render_template("goodbye.html")
+        return render_template("goodbye.html", person=session['username'])
 
 @app.route("/madlib", methods=['POST'])
 def show_madlib():
